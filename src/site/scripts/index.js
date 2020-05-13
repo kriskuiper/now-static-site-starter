93a9456ce4f67826c09f885ba75959121cb0b062
Kris <kriskuiper74@gmail.com>
2020-05-13 15:05:39 +0200

    feat: make now serverless functions work

--- src/site/scripts/index.js
+++ src/site/scripts/index.js
@@ -1,1 +1,8 @@
 console.log('Works 👍')
+
+// Check if the serverless function is working
+fetch('/api/hello')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.message)
+  })
